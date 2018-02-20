f4292a81e7fbff79e1157708e9c0d16ad2d9aca0
Victor <victorherrerocazurro@gmail.com>
2018-02-20 19:52:39 +0100

    Maven

---
+++ workspace/01_HolaMundo/src/test/java/com/curso/maven/holamundo/DummyAT.java
@@ -0,0 +1,14 @@
+package com.curso.maven.holamundo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyAT {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
